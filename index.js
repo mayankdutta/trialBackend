c84c77f55cd6ac17f10920beceeeb2145e8e94f3
mayank dutta <smayankdutt@gmail.com>
2022-04-20 00:16:07 +0530

    add product backend schema done

--- index.js
+++ index.js
@@ -3,22 +3,49 @@ const cors = require("cors");
 
 require("./db/config");
 const User = require("./db/User");
+const Product = require("./db/Products")
 
 const app = express();
 
-// post, req, will be in JSON format
 app.use(express.json());
 
 app.use(cors());
 
-// request body
-// res is something we send
-// req is something we receive
-
 app.post("/register", async (req, res) => {
-  let use = new User(req.body);
-  let result = await use.save();
+  let user = new User(req.body);
+  let result = await user.save();
+
+  // removing password field from the register page as well.
+  // couldn't use .select as we did below because this ain't mongo db
+  result = result.toObject();
+  delete result.password;
+
   res.send(result);
 });
+
+app.post("/login", async (req, res) => {
+  // removed password from the login
+  let user = await User.findOne(req.body).select("-password");
+  if (req.body.password && req.body.email) {
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({
+        result: "no user found",
+      });
+    }
+  } else {
+    res.send({
+      result: "no user found",
+    });
+  }
+});
+
+app.post("/add-product", async (req, res) => {
+  let product = new Product(req.body);
+  let result = await product.save()
+  res.send(result);
+
+})
 
 app.listen(5000, () => {});
